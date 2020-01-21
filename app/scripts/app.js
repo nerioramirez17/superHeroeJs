7d8fce859fc67682ac99d078a03b5667e59db33b
Nerio Ramirez <nerioramirez17@gmail.com>
2020-01-21 18:45:16 -0300

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+
+angular
+  .module('tareasApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/home', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl',
+        controllerAs: 'home'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
